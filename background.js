09b1b17c47173bd547b6777c82b8ccd8b9058e6a
dddmmm789 <33433162+dddmmm789@users.noreply.github.com>
2024-08-21 13:44:37 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,32 +1,29 @@
-let logs = [];
-
-function log(...args) {
-  console.log(...args);
-  logs.push(args.map(arg => JSON.stringify(arg)).join(' '));
+function sendLogoutMessage(tabId, url, retries = 3) {
+  chrome.tabs.sendMessage(tabId, { action: "logout" }, (response) => {
+    if (chrome.runtime.lastError) {
+      console.log("Error sending message to tab:", chrome.runtime.lastError);
+      if (retries > 0) {
+        console.log("Retrying in 1 second...");
+        setTimeout(() => sendLogoutMessage(tabId, url, retries - 1), 1000);
+      }
+    } else {
+      console.log("Message sent successfully to tab:", tabId);
+    }
+  });
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  log("Message received in background:", request);
+  console.log("Message received in background:", request);
   if (request.action === "triggerLogout") {
     chrome.tabs.query({}, (tabs) => {
-      log("Number of tabs found:", tabs.length);
+      console.log("Number of tabs found:", tabs.length);
       tabs.forEach((tab) => {
-        log("Tab URL:", tab.url || "undefined");
+        console.log("Tab URL:", tab.url || "undefined");
         if (tab.url && tab.url.match(/facebook\.com|twitter\.com|instagram\.com|linkedin\.com/)) {
-          log("Sending logout message to tab:", tab.id, tab.url);
-          chrome.tabs.sendMessage(tab.id, { action: "logout" }, (response) => {
-            if (chrome.runtime.lastError) {
-              log("Error sending message to tab:", chrome.runtime.lastError);
-            } else {
-              log("Message sent successfully to tab:", tab.id);
-            }
-          });
+          console.log("Sending logout message to tab:", tab.id, tab.url);
+          sendLogoutMessage(tab.id, tab.url);
         }
       });
     });
-  } else if (request.action === "getLogs") {
-    sendResponse({logs: logs.join('\n')});
-    logs = []; // Clear logs after sending
   }
-  return true; // Indicates that the response will be sent asynchronously
 });
