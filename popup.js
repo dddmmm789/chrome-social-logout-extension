4714f2edd9bc6e12752698f9a4c05ec72aa64ffc
dddmmm789 <33433162+dddmmm789@users.noreply.github.com>
2024-08-21 14:01:00 +0300

    Update popup.js
    no logs

--- popup.js
+++ popup.js
@@ -1,15 +1,17 @@
 document.getElementById('logoutButton').addEventListener('click', () => {
-    console.log("Logout button clicked");
-    chrome.runtime.sendMessage({ action: "triggerLogout" }, (response) => {
-      console.log("Response from background:", response);
-    });
+  console.log("Logout button clicked");
+  chrome.runtime.sendMessage({ action: "triggerLogout" }, (response) => {
+    console.log("Response from background:", response);
   });
-  
-  document.getElementById('getLogs').addEventListener('click', () => {
-    chrome.runtime.sendMessage({action: "getLogs"}, (response) => {
-      console.log(response.logs);
-      navigator.clipboard.writeText(response.logs).then(() => {
-        alert("Logs copied to clipboard!");
-      });
-    });
+});
+
+document.getElementById('getLogs').addEventListener('click', () => {
+  chrome.runtime.sendMessage({action: "getLogs"}, (response) => {
+    console.log("Logs received:", response);
+    if (response && response.logs) {
+      document.getElementById('logsArea').textContent = response.logs;
+    } else {
+      document.getElementById('logsArea').textContent = "No logs available";
+    }
   });
+});
