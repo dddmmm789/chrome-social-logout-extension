a0f25dfeb0d7ae53c68cd8532d3c3d025e7370ed
dddmmm789 <33433162+dddmmm789@users.noreply.github.com>
2024-08-21 14:00:39 +0300

    Update background.js
    no logs

--- background.js
+++ background.js
@@ -1,29 +1,43 @@
+let logs = [];
+
+function log(...args) {
+  console.log(...args);
+  logs.push(args.map(arg => JSON.stringify(arg)).join(' '));
+}
+
 function sendLogoutMessage(tabId, url, retries = 3) {
   chrome.tabs.sendMessage(tabId, { action: "logout" }, (response) => {
     if (chrome.runtime.lastError) {
-      console.log("Error sending message to tab:", chrome.runtime.lastError);
+      log("Error sending message to tab:", chrome.runtime.lastError.message);
       if (retries > 0) {
-        console.log("Retrying in 1 second...");
+        log("Retrying in 1 second...");
         setTimeout(() => sendLogoutMessage(tabId, url, retries - 1), 1000);
       }
     } else {
-      console.log("Message sent successfully to tab:", tabId);
+      log("Message sent successfully to tab:", tabId);
     }
   });
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log("Message received in background:", request);
+  log("Message received in background:", request);
   if (request.action === "triggerLogout") {
     chrome.tabs.query({}, (tabs) => {
-      console.log("Number of tabs found:", tabs.length);
+      log("Number of tabs found:", tabs.length);
       tabs.forEach((tab) => {
-        console.log("Tab URL:", tab.url || "undefined");
+        log("Tab URL:", tab.url || "undefined");
         if (tab.url && tab.url.match(/facebook\.com|twitter\.com|instagram\.com|linkedin\.com/)) {
-          console.log("Sending logout message to tab:", tab.id, tab.url);
+          log("Sending logout message to tab:", tab.id, tab.url);
           sendLogoutMessage(tab.id, tab.url);
         }
       });
     });
+  } else if (request.action === "getLogs") {
+    log("Sending logs");
+    sendResponse({logs: logs.join('\n')});
+    logs = []; // Clear logs after sending
   }
+  return true; // Indicates that the response will be sent asynchronously
 });
+
+log("Background script loaded");
